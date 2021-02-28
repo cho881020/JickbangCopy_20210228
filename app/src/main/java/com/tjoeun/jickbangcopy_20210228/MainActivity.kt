fce8289827dcf83d33e49f09b5cef3420ce85f50
조경진 <cho881020@gmail.com>
2021-02-28 12:36:55 +0900

    실제 방 데이터 추가  => 설명 문구만 미비 (작업 너무 번거로움)

--- app/src/main/java/com/tjoeun/jickbangcopy_20210228/MainActivity.kt
+++ app/src/main/java/com/tjoeun/jickbangcopy_20210228/MainActivity.kt
@@ -2,10 +2,28 @@ package com.tjoeun.jickbangcopy_20210228
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tjoeun.jickbangcopy_20210228.datas.Room
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<Room>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add(Room(8000, "서울시 종로구", 4, "종로의 4층 방."))
+        mRoomList.add(Room(25600, "경기도 고양시", -1, "고양시의 지하1층 방."))
+        mRoomList.add(Room(17000, "서울시 동작구", 0, "종로의 4층 방."))
+        mRoomList.add(Room(4000, "충청시 천안시", 5, "종로의 4층 방."))
+        mRoomList.add(Room(5000, "서울시 은평구", 8, "종로의 4층 방."))
+        mRoomList.add(Room(35000, "서울시 동대문구", -2, "종로의 4층 방."))
+        mRoomList.add(Room(8000, "서울시 강북구", 3, "종로의 4층 방."))
+        mRoomList.add(Room(2500, "경기도 과천시", 1, "종로의 4층 방."))
+        mRoomList.add(Room(78000, "경기도 고양시", 0, "종로의 4층 방."))
+        mRoomList.add(Room(160000, "서울시 종로구", -4, "종로의 4층 방."))
+        mRoomList.add(Room(9900, "서울시 은평구", 8, "종로의 4층 방."))
+
     }
 }
